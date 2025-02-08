b85807d439a72cf26d4b17d1deab8406aae75785
FidanZeynalova <fidanhz-ap206@code.edu.az>
2025-02-08 13:27:43 +0400

    day-5

--- backend/config/Config.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const app = express();
-app.use(express.json());
-const mongoose = require("mongoose")
-
-mongoose.connect(process.env.MONGODB_URL)
-    .then(() => console.log("✅ Successfully connected to Half Baked Harvest Database"))
-    .catch((err) => console.log("❌ Failed to connect to database:", err));
